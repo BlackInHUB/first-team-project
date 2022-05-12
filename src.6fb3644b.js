de6f4edf8532849d109230e8f99f72139f383ff6
BlackInHUB <96769199+BlackInHUB@users.noreply.github.com>
2022-05-12 08:40:43 +0000

    Deploying to gh-pages from @ BlackInHUB/first-team-project@fc2d0c92a76b94f9762f8d059d65607e6162642e 🚀

--- src.6fb3644b.js
+++ src.6fb3644b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/first-team-project/src.fde6761f.js.map
+//# sourceMappingURL=/first-team-project/src.6fb3644b.js.map
