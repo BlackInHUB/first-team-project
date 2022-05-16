e9f3909574bbe20851de4f250f067e02014008f9
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-05-16 20:03:07 +0300

    Update js

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,6 +1,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-window-open]"),
+    openModalBtn: document.querySelector("[data-modal-window-open2]"),
     closeModalBtn: document.querySelector("[data-modal-window-close]"),
     modal: document.querySelector("[data-modal-window]"),
   };
